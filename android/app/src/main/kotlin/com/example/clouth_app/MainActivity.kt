9aab76cb11593b891301ce2ae190881d0dc6044c
Ravinder badesra <rsbadesra3899@gmail.com>
2023-10-14 11:55:53 +0530

    first uploud

---
+++ android/app/src/main/kotlin/com/example/clouth_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clouth_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:clouth_app/users/authfication/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FutureBuilder(
+        builder: (context, snapshot) {
+          return LoginScreen();
+        },
+        future: null,
+      ),
+    );
+  }
+}
